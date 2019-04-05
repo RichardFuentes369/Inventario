055d0cdd694c00b1e2e029127e55fd1ec8fe1de9
RichardFuentes369 <javierbaron6@gmail.com>
2019-04-05 04:03:03 -0500

    Advertencia de Loguin

--- resources/views/home/views/index.blade.php
+++ resources/views/home/views/index.blade.php
@@ -1,5 +1,20 @@
 @extends('Plantillas.puser')
 
 @section('content')
-	@include('home.form.form-login')
+	@if(Auth::check() == true)
+	<div class="col-sm-12">
+		<h3 align="left"><strong>Aviso</strong></h3>
+	</div>
+	<hr>
+		<center>
+			<h2>Por tu seguridad y la de la empresa <br> recuerda siempre cerrar tu session <br> cuando no te encuentres frente a la monitor
+			</h2>
+			<form action="{{ url('index') }}/{{ ('Logout') }}">
+				<button class="btn btn-danger">Salir</button> <br><br>		
+			</form>
+		</center>
+	<hr>
+	@else
+		@include('home.form.form-login')
+	@endif
 @endsection
